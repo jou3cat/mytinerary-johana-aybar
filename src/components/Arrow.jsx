329af8a9167b2c25ccfa7baaadebd76c7b97b5b6
Johana <joha.aybar@gmail.com>
2023-08-10 01:30:03 -0300

    Uso de state en Navegador y carrousel

--- src/components/Arrow.jsx
+++ src/components/Arrow.jsx
@@ -1,12 +1,12 @@
 
 import React from 'react'
 
-export default function Arrow({direction}) {
+export default function Arrow({direction, onClick}) {
 
 
   return (
 
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 cursor-pointer" onClick={onClick}>
     <path strokeLinecap="round" strokeLinejoin="round" d={direction} />
     </svg>
   
