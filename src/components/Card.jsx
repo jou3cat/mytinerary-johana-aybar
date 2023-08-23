00de8a33ee69dafad6340986b59ab560360a467e
Johana <joha.aybar@gmail.com>
2023-08-22 23:33:16 -0300

    use effect

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Card({src, alt, text_city, text_country}) {
+export default function Card({src, alt, text}) {
   return (
     <div className='w-2/5 flex flex-col items-center p-2 n-2 space-x-1 rounded-sm border-gray-700'>
       <img src={src} alt={alt} />
-      <p>{text_city} - {text_country}</p>
+      <p>{text} </p>
     </div>
   )
 }
--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
 import Arrow from './Arrow'
 import Card from './Card'
 import { useState } from 'react'
 
-export default function Carrousel({cities}) {
-let image=0
-let toImage=image+4
+export default function Carrousel({data}) {
 
   let [counter, setCounter]= useState(0)
   let [counterTo, setCounterTo]= useState(4)
     
   function next_slide(){
 
-      if(cities.length===counterTo){
+      if(data.length<=counterTo){
         setCounter(0)
         setCounterTo(4)
       }else{
@@ -24,21 +21,21 @@ let toImage=image+4
 
   function previous_slide(){
     
-    if(counter===0){
-      setCounter(cities.length -4)
-      setCounterTo(cities.length)
+    if(counter<=0){
+      setCounter(data.length -4)
+      setCounterTo(data.length)
     }else{
       setCounter(counter - 4)
       setCounterTo(counterTo - 4)
     }
-
+  
   }
 
   return (
     <div className='flex justify-center items-center'>
      <Arrow direction="M15.75 19.5L8.25 12l7.5-7.5" onClick={previous_slide} />
      <div className='flex w-11/12 flex-wrap justify-center mt-5' >
-      {cities.slice(counter,counterTo).map(each=> <Card src={each.photo} alt={each.id} text_city={each.city} text_country={each.country} />)}
+      {data.slice(counter,counterTo).map(each=> <Card src={each.photo} alt={each.id} text={each.city} />)}
     
      </div>
       <Arrow direction="M8.25 4.5l7.5 7.5-7.5 7.5" onClick={next_slide} />
