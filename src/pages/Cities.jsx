3e09e32c54c3553e792fa73682153450a82740b3
jou3cat <110557681+jou3cat@users.noreply.github.com>
2023-08-31 23:50:04 -0300

    Cambios (#5)
    * modifico
    
    * Cambios para completar el sprint1
    
    * Agrego estilos a la página Cities

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -1,28 +1,52 @@
-import React, { useState, useEffect, useRef } from 'react'
-import axios from 'axios'
-import apiUrl from '../apiUrl'
-
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import apiUrl from '../apiUrl';
+import CityCard from '../components/CityCard.jsx';
 
 export default function Cities() {
-  const [cities,setCities]=useState([])
-  const text=useRef()
-  const [reEffect, setReEffect]=useState(true)
-  useEffect(
-    ()=>{  
-      axios(apiUrl + 'cities?city=' + text.current.value)
-      //.then(res=>console.log(res.data.response))
-      .then(res=>setCities(res.data.response))
-      .catch(err=>console.log(err))
-    },[reEffect]
-  )
-  function handleFilter(){
-console.log(text.current.value);
-setReEffect(!reEffect)
+  const [cities, setCities] = useState([]);
+  const text = useRef();
+  const [reEffect, setReEffect] = useState(true);
+
+  useEffect(() => {
+    axios(apiUrl + 'cities?city=' + text.current.value)
+      .then((res) => setCities(res.data.response))
+      .catch((err) => console.log(err));
+  }, [reEffect]);
+
+  function handleFilter() {
+    setReEffect(!reEffect);
   }
+
   return (
     <>
-    <input ref={text} type="text" name="text" id="text" onKeyUp={handleFilter} />
-    {  cities.map(each=> <p className='text-white' Key={each.id}>  {each.city} </p> )}
+      <div className='w-full font-bold text-center text-white p-2'>
+          <h1 className='text-3xl md:text-4xl lg:text-5xl p-4'>Cities</h1>
+          <h2 className='p-2'>Collection of the most beautiful places and experiences</h2>
+      </div>
+
+      <div id='Buscador' className="container mx-auto" >
+        <input
+          ref={text}
+          type="text"
+          name="text"
+          id="text"
+          className="w-full p-2 mb-4 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring focus:border-blue-300"
+          onKeyUp={handleFilter}
+          placeholder="Search cities"
+        />
+        <div className=" flex flex-wrap">
+          {cities.map((each) => (
+            <CityCard key={each.id} city={each}
+            src={each.photo}
+            alt={each.id}
+            text={each.city}
+            id={each.id}
+
+            />
+          ))}
+        </div>
+      </div>
     </>
-  )
+  );
 }
